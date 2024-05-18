525405a0be25841f6efb6da745c003241ff9af16
Thanuravi01 <thanuravi0109@gmail.com>
2024-05-18 15:53:42 +0530

    Removed

--- Test/test_register.py
+++ Test/test_register.py
@@ -10,7 +10,7 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
-class TestLogin():
+class TestRegister():
   def setup_method(self, method):
     self.driver = webdriver.Chrome()
     self.vars = {}
@@ -18,19 +18,15 @@ class TestLogin():
   def teardown_method(self, method):
     self.driver.quit()
   
-  def test_login(self):
+  def test_register(self):
     self.driver.get("http://localhost:8082/")
     self.driver.set_window_size(1382, 744)
-    self.driver.find_element(By.CSS_SELECTOR, ".profile-dropdown img").click()
-    self.driver.find_element(By.LINK_TEXT, "Logout").click()
-    self.driver.find_element(By.LINK_TEXT, "Sign up").click()
-    self.driver.find_element(By.NAME, "username").click()
-    self.driver.find_element(By.NAME, "username").send_keys("Ram")
-    self.driver.find_element(By.NAME, "email").click()
-    self.driver.find_element(By.NAME, "email").send_keys("ram@gmail.com")
-    self.driver.find_element(By.ID, "password1").click()
-    self.driver.find_element(By.ID, "password1").send_keys("ram123")
-    self.driver.find_element(By.ID, "password2").click()
-    self.driver.find_element(By.ID, "password2").send_keys("ram123")
+    self.driver.find_element(By.LINK_TEXT, "Recipes").click()
+    self.driver.find_element(By.LINK_TEXT, "Feedback").click()
+    self.driver.find_element(By.ID, "recipe_name").click()
+    self.driver.find_element(By.ID, "recipe_name").send_keys("Vegetable Pulao")
+    self.driver.find_element(By.CSS_SELECTOR, "label:nth-child(8)").click()
+    self.driver.find_element(By.ID, "feedback").click()
+    self.driver.find_element(By.ID, "feedback").send_keys("Ver good explanation and easy to follow")
     self.driver.find_element(By.CSS_SELECTOR, "button").click()
   
